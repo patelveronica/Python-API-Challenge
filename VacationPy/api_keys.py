a3cb69b28a89df66f714cd687c09576895cd6f00
veronika patel <85206817+patelveronica@users.noreply.github.com>
2021-07-22 18:10:13 -0400

    Update api_keys.py

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "87ac0f45fbc8d9105aaa68d3781039eb"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDjAGdDXDe3nkHHk-2aSWQE1zRLVxP5i9I"
+g_key = ""
