b949a23b6758d7b2b01f24b509513448f41f9f20
patelveronica <85206817+patelveronica@users.noreply.github.com>
2021-07-22 16:20:52 -0400

    adding Vacationpy working file

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "87ac0f45fbc8d9105aaa68d3781039eb"
 
 # Google API Key
 g_key = "AIzaSyDjAGdDXDe3nkHHk-2aSWQE1zRLVxP5i9I"
